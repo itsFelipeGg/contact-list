5256f8f7cad5c823bc43ea407ab43dcf6879cd49
Felipe Estupiñan <nickyfeliper@gmail.com>
2023-09-28 17:09:36 -0500

    se agg los contactos y fyunciones

---
+++ contact-list.js
@@ -0,0 +1,11 @@
+let listacontacto = [
+  "alexis marquez",
+  "mauricio restrepo",
+  "ethan romero",
+  "hamminton piñeros",
+  "manuela zuluaga",
+];
+
+function agregarcontactos(nuevocontacto) {
+  listacontacto.push(nuevocontacto);
+}
